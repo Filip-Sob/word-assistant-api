e67d23a6718b88eb6728cdde58c718ab0d8a865a
Filip Sobczak <filip.sobbczak@gmail.com>
2025-11-11 18:59:58 +0100

    Initial project setup: Spring Boot + PostgreSQL configuration

---
+++ src/main/java/pl/sobczak/wordassistant/WordAssistantApiApplication.java
@@ -0,0 +1,13 @@
+package pl.sobczak.wordassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordAssistantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordAssistantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sobczak/wordassistant/WordAssistantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sobczak.wordassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordAssistantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
